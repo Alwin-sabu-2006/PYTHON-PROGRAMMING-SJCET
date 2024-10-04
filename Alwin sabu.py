74ff25f56ccf2fd88207f92efd75c98ef4967473
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-10-04 16:05:18 +0530

    Add files via upload

---
+++ Alwin sabu.py
@@ -0,0 +1,44 @@
+'''print("hello\nworld")
+programme to check whether the given no is greatest
+if 5>6:
+    print("Five is greater than two")
+if  5>2:
+    print("it is the greatest")
+x="hello"
+print(x)
+y=5
+print(y)
+
+x=5
+x=6
+print(x)
+x=int(3)
+y=float(5)
+z=str(4)
+print(x,y,z)
+print(x)
+print(y)
+print(z)
+
+x="alwin"
+print(type(x))
+y=5.8
+print(type(y))
+
+
+x,y,z = "orange" ,"ban","apple"
+print(x,y,z)
+x="python "
+y="is "
+z="awesome"
+print(x+y+z)
+n=input("enter your name")
+print("the username is:",n)
+
+s="hello "
+b="good morning"
+c=s+b
+print(c)
+print(c[-1]) '''
+v="hallooo"
+print(v[1:3])
