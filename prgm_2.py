a6791534465816518246a497607251bc6a6aa33b
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-10-18 15:05:18 +0530

    Add files via upload

---
+++ prgm_2.py
@@ -0,0 +1,11 @@
+'''
+Author=ALWIN SABU
+DATE:18/10/2006
+Description:a Python program that stores a string in a variable.
+Extract a specific part of the string (substring) and
+then concatenate it with another string. Finally, display the new string.
+'''
+string1="Hello, World!"
+string2=" Everyone!"
+str3=string1[6:12]
+print(str3+string2)
