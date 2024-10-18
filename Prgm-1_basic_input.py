d1165b7abe51686e3fe2c0f847979adc6061c4d4
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-10-18 14:29:09 +0530

    Add files via upload

---
+++ Prgm-1_basic_input.py
@@ -0,0 +1,10 @@
+'''
+Author:Alwin Sabu
+Date:18/10/2024
+ Description: a Python program that asks the user to input their name and age.
+ After receiving the input, display a message that includes the user's name and age
+ output=Hello,John! YOUR are 25 years old.
+'''
+x=input("enter your name:")
+y=int(input("enter your age:"))
+print("HELLO,",x+'!You are',y,'years old.')
