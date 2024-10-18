94f76146f3a9c9fcc868b91d139fcd92b15e0692
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-10-18 16:18:22 +0530

    Add files via upload

---
+++ prgm4 math module.py
@@ -0,0 +1,11 @@
+'''
+Author=ALWIN SABU
+DATE=18/10/24
+description=a Python program that uses functions
+from the math module to perform the following operations on a number provided by the user:
+'''
+import math
+num1=int(input("Enter a number"))
+print("Sqrt of",num1,":",math.sqrt(num1))
+print("Factorial of ",num1,":",math.factorial(num1))
+print(num1,"raise to power 2:",math.pow(num1,2))
