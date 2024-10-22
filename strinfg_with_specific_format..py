65eeb055bd176dcd7a6653646ee8e6787ef76dca
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-10-22 19:33:54 +0530

    string_with_specific_format..py

--- strinfg_with_specific_format..py
+++ strinfg_with_specific_format..py
@@ -1,2 +1,2 @@
-"Author=ALWIN SABU date:22/10/24 description="a prgm that uses various tabs" 
+"Author=ALWIN SABU date:22/10/24 description=a prgm that uses various tabs" 
 print("Twinkle,twinkle\tlittle star,\n\t\t\tHow I wonder what you are!\n\t\t\t\tUp above the world so high,\n\t\t\t\tLike a diamond in the sky.\nTwinkle, twinkle,little star,\n \t\tHow I wonder what you are")
