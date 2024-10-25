fd35ea016b5037e42e23fc11d1bed0e6b8435deb
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-10-25 14:42:20 +0530

    Add files via upload

---
+++ POSITIVE_OR_NEGATIVE.py
@@ -0,0 +1,12 @@
+'''
+AUTHOR=ALWIN SABU
+DATE:25/10/24
+DESCRIPTION:a Python program to check wheher a number is positive or not
+'''
+num1=int(input("Enter the number:"))
+if num1>0:
+    print("THe number is positive")
+elif num1==0:
+    print("The number You have entered is Zero")
+else:
+    print("The number is negative")
