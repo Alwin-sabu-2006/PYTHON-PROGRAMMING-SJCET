b2cd6211822fc126a17c8efa3f3c77028002a3b5
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-11-29 16:30:21 +0530

    Add files via upload

---
+++ recursive_divisor.py
@@ -0,0 +1,9 @@
+def GCD(num1,num2):
+    if num2==0:
+        return num1
+    else:
+        return GCD(num2,num1%num2)
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number"))
+if numj1.0:
+    if
